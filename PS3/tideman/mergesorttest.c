6b2f44046963732a7f447a4230863bfc1cf269f5
linedoestrolling <zhaoyiheng@DESKTOP-RV9AMD8.localdomain>
2022-01-08 19:35:15 +0800

    testing ubuntu

--- PS3/tideman/mergesorttest.c
+++ PS3/tideman/mergesorttest.c
@@ -16,106 +16,112 @@ int main (void) {
     arr[6] = 8;
     arr[7] = 7;
     merge_sort(arr, 0, 7, 8);
-    //test
 }
 
-void merge_sort (int input[], int p, int r, int size) { 
-    printf("p is %i\n", p);
-    printf("r is %i\n", r);
+void merge_sort (int input[], int start, int end, int size) {
+    printf("start_index is %i\n", start);
+    printf("end_index is %i\n", end);
     if (size <= 1) {
         printf("reach end case\n");
         return;
     }
+
+    //test
     else {
-        int q = (p+r)/2;
-        int left_half[q - p + 1];
-        int right_half[r - q];
+        int middle  = (start+end)/2;
 
-        for (int i = p; i <= q; i++) { //splitting the array into 2 sub arrays
+        printf("middle_index is %i\n", middle);
+
+        merge_sort(input, start, middle, middle - start + 1);
+
+
+        merge_sort(input, middle + 1, end, end - middle);
+
+        //making the 2 subarrays
+        int left_half[middle - start + 1];
+        int right_half[end - middle];
+        int left_array_size = 0;
+        int right_array_size = 0;
+
+        for (int i = start; i <= middle; i++) { //splitting the array into 2 sub arrays
             left_half[i] = input[i];
+            left_array_size++;
         }
 
-        for (int i = q+1; i < q; i++) {  // second sub array
+        for (int i = middle+1; i < end; i++) {  // second sub array
             right_half[i] = input[i];
+            right_array_size++;
         }
-
-        printf("q is %i\n", q);
-        merge_sort(left_half, 0, q - p, q - p + 1);
-        merge_sort(right_half, 0, r - q - 1, r - q);
 
         //merging
-        int a = 0;
-        int b = 0;
-        int c = 0;
-        printf("a initialized as : %i\n", a);
-        printf("b initialized as : %i\n", b);
-        printf("c initialized as : %i\n", c);
-        printf("r initialized as : %i\n", r);
-        printf("q is: %i\n", q);
-        int temp_array[r - p + 1];
-        for (int i = 0; i < r - p + 1; i++) {
-            temp_array[i] = 0;
-        }
 
-        int left_half_size = sizeof(left_half);
-        int right_half_size = sizeof(right_half);
+        int left_array_index = start;
+        int right_array_index = middle + 1;
+        int main_index = start;
 
-        while (a <= left_half_size && b <= right_half_size) {
-            if (left_half[a] > right_half[b]) {
-                printf("a is : %i\n", a);
-                printf("c is : %i\n", c);
-                printf("element is %i\n", left_half[a]);
-                temp_array[c] = left_half[a];
-                a++;
-                c++;
+        printf("left index initialized as : %i\n", left_array_index);
+        printf("right index initialized as : %i\n", right_array_index);
+
+        printf("middle_index is: %i\n\n", middle);
+
+        // for (int i = 0; i < end - start + 1; i++) {
+        //     temp_array[i] = 0;
+        // }
+
+        while (left_array_index <= middle && right_array_index <= end) {
+            if (left_half[left_array_index] > right_half[right_array_index]) {
+                printf("left index is : %i\n", left_array_index);
+                printf("main index is : %i\n", main_index);
+                printf("element is %i\n", input[left_array_index]);
+                input[main_index]  = left_half[left_array_index];
+                left_array_index++;
+                main_index++;
             }
-            else if (right_half[b] > left_half[a]) {
-                printf("b is : %i\n", b);
-                printf("c is : %i\n", c);
-                printf("element is %i\n", right_half[b]);
-                temp_array[c] = right_half[b];
-                b++;
-                c++;
+            else if (right_half[right_array_index ] > left_half[left_array_index]) {
+                printf("right index is : %i\n", right_array_index);
+                printf("main index is : %i\n", main_index);
+                printf("element is %i\n", right_half[right_array_index]);
+                input[main_index] = right_half[right_array_index];
+                right_array_index++;
+                main_index++;
             }
             else {
-                printf("a is : %i\n", a);
-                printf("b is : %i\n", b);
-                printf("c is : %i\n", c);
-                printf("element 1 is %i\n", left_half[a]);
-                printf("element 2 is %i\n", right_half[b]);
-                temp_array[c] = left_half[a];
-                a++;
-                c++;
-                temp_array[c] = right_half[b];
-                b++;
-                c++;
+                printf("left index is : %i\n", left_array_index);
+                printf("right index is : %i\n", right_array_index);
+                printf("main index is : %i\n", main_index);
+                printf("element 1 is %i\n", left_half[left_array_index]);
+                printf("element 2 is %i\n", right_half[right_array_index]);
+                input[main_index] = left_half[left_array_index];
+                left_array_index++;
+                main_index++;
+                input[main_index] = right_half[right_array_index];
+                right_array_index++;
+                main_index++;
             }
         }
 
-        while (a <= left_half_size) {
+        while (left_array_index <= left_array_size) {
             printf("grabbing the rest in first half\n");
-            temp_array[c] = left_half[a];
-            printf("grabbed element is: %i\n", input[a]);
-            a++;
-            c++;
+            input[main_index] = left_half[left_array_index];
+            printf("grabbed element is: %i\n", left_half[left_array_index]);
+            left_array_index++;
+            main_index++;
         }
-        while (b <= right_half_size) {
+        while (right_array_index <= right_array_size) {
             printf("grabbing the rest in second half\n");
-            temp_array[c] = input[b];
-            printf("grabbed element is: %i\n", input[b]);
-            b++;
-            c++;
+            input[main_index] = right_half[right_array_index];
+            printf("grabbed element is: %i\n", right_half[right_array_index]);
+            right_array_index++;
+            main_index++;
         }
 
-        
-        for (int i = 0; i < (r - p + 1); i++) {
-            input[i] = temp_array[i];
-        } 
-        
-        for (int i = 0; i < (r - p + 1); i++) {
+
+
+        for (int i = 0; i < (end - start + 1); i++) {
             printf("input element is: %i\n", input[i]);
         }
 
 
     }
 }
+
